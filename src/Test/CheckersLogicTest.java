2efc65a8ef9ffb46a0af4b7fdf1c6ca694b49e9d
Ian Skelskey <46094112+IanSkelskey@users.noreply.github.com>
2021-12-13 14:49:00 -0700

    Add files via upload

--- src/Test/CheckersLogicTest.java
+++ src/Test/CheckersLogicTest.java
@@ -1,179 +1,202 @@
-package Test;
+package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Point;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Assert;
 
 import core.CheckersLogic;
 
-/**
- * CheckersLogicTest Class
- * Tests all major methods of CheckersLogic Class
- * @author Ian Skelskey
- * @version 1.0
- * @since 10-7-2021
- */
 class CheckersLogicTest {
+	//Constants
+	private final int BOARD_WIDTH = 8, BOARD_HEIGHT = 8;
+	private final char BLANK = '_', RED = 'o', BLUE = 'x';
 	
-	CheckersLogic testGame;
+	private static char[][] testBoard;
 	
-	/**
-	 * @throws java.lang.Exception
-	 */
+	private static CheckersLogic testGame;
+	
 	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("setUpBeforeClass() is running...");
+		testGame = new CheckersLogic();
+
+	}
+	
+	@AfterAll
+	public static void tearDownAfterClass() throws Exception {
 		
 	}
+	
+//	@BeforeEach
+//	public static void setup() throws Exception {
+//		
+//	}
+//	
+//	@AfterEach
+//	public static void tearDown() throws Exception {
+//		
+//	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeEach
-	void setUp() throws Exception {
-		System.out.println("Before eaching...");
-		CheckersLogic testGame = new CheckersLogic();
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	/** Test method for {@link core.CheckersLogic#CheckersLogic()}. */
 	@Test
-	void testCheckersLogic() {
-		System.out.println("testCheckersLogic()...");
+	void testInitBoard() {
+		int xCount = 0;
+		int oCount = 0;
+		testGame.initBoard();
+		testBoard = testGame.getBoard();
+		for(int i = 0; i < BOARD_HEIGHT; i++) {
+			for(int j = 0; j < BOARD_WIDTH; j++) {
+				System.out.print(testBoard[i][j]);
+				if((i+j) % 2 == 1) {
+					if(testBoard[i][j] == RED) {
+						oCount += 1;
+					}else if(testBoard[i][j] == BLUE) {
+						xCount += 1;
+					}
+				}
+			}
+			System.out.print("\n");
+		}
+		System.out.println("x count: " + xCount + "; o count: " + oCount);
+		assertEquals(oCount, 12);
+		assertEquals(xCount, 12);
+		assertEquals(testGame.boardRows(), BOARD_HEIGHT);
+		assertEquals(testGame.boardColumns(), BOARD_WIDTH);
 	}
 
-	/** Test method for {@link core.CheckersLogic#getBoard()}. */
+	@Test
+	void testMovablePieces() {
+		fail("Not yet implemented");
+	}
+
 	@Test
 	void testGetBoard() {
-		System.out.println("testGetBoard()...");
-
-	}
-
-	/** Test method for {@link core.CheckersLogic#move(int, int, int, int)}. */
-	@Test
-	void testMove() {
 		fail("Not yet implemented");
 	}
 
-	/** Test method for {@link core.CheckersLogic#getMoveType(int)}. */
 	@Test
-	void testGetMoveType() {
+	void testBoardRows() {
+		assertEquals(testGame.boardRows(), BOARD_HEIGHT);
+	}
+
+	@Test
+	void testBoardColumns() {
 		fail("Not yet implemented");
 	}
 
-	/** Test method for {@link core.CheckersLogic#turn()}. */
+	@Test
+	void testBoardPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBoardContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckMoves() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPositionForMoveFrom() {
+		fail("Not yet implemented");
+	}
+
 	@Test
 	void testTurn() {
 		fail("Not yet implemented");
 	}
 
-	/** Test method for {@link core.CheckersLogic#isOver()}. */
+	@Test
+	void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMovePiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeletePiece() {
+
+		testGame.initBoard();
+		testBoard = testGame.getBoard();
+		for(int i = 0; i < BOARD_HEIGHT; i++) {
+			for(int j = 0; j < BOARD_WIDTH; j++) {
+				if(testBoard[i][j] == RED) {
+					System.out.println("Deleting piece at: (" + j + ", " + i +") owned by : x");
+					testGame.deletePiece(RED, new Point(j,i));
+				}else if(testBoard[i][j] == BLUE) {
+					System.out.println("Deleting piece at: (" + j + ", " + i +") owned by : o");
+					testGame.deletePiece(BLUE, new Point(j,i));
+				}
+				assertTrue(testBoard[i][j] == BLANK);
+			}
+		}
+		//Why are these not empty now? 
+		System.out.println(testGame.getPieces(BLUE));
+		System.out.println(testGame.getPieces(RED));
+
+		assertTrue(testGame.getPieces(BLUE).isEmpty());
+		assertTrue(testGame.getPieces(RED).isEmpty());
+	}
+
 	@Test
 	void testIsOver() {
-		fail("Not yet implemented");
+		assertFalse(testGame.isOver());
 	}
 
-	/**
-	 * Test method for {@link core.CheckersLogic#setActivePlayer(core.CheckersPlayer)}.
-	 */
 	@Test
-	void testSetActivePlayer() {
+	void testCanPieceMove() {
 		fail("Not yet implemented");
 	}
 
-	/** Test method for {@link core.CheckersLogic#getActivePlayer()}. */
 	@Test
-	void testGetActivePlayer() {
+	void testGetInstructions() {
 		fail("Not yet implemented");
 	}
 
-	/** Test method for {@link core.CheckersLogic#switchPlayer()}. */
 	@Test
-	void testSwitchPlayer() {
+	void testGetMoveType() {
 		fail("Not yet implemented");
 	}
 
-	/** Test method for {@link core.CheckersLogic#passInput(java.lang.String)}. */
-	@Test
-	void testPassInput() {
-		fail("Not yet implemented");
-	}
-
-	/** Test method for {@link core.CheckersLogic#clearInput()}. */
-	@Test
-	void testClearInput() {
-		fail("Not yet implemented");
-	}
-
-	/** Test method for {@link core.CheckersLogic#splitInstructions()}. */
-	@Test
-	void testSplitInstructions() {
-		fail("Not yet implemented");
-	}
-
-	/** Test method for {@link core.CheckersLogic#asRow(int)}. */
-	@Test
-	void testAsRow() {
-		fail("Not yet implemented");
-	}
-
-	/** Test method for {@link core.CheckersLogic#asCol(char)}. */
-	@Test
-	void testAsCol() {
-		fail("Not yet implemented");
-	}
-
-	/** Test method for {@link core.CheckersLogic#validateMove()}. */
 	@Test
 	void testValidateMove() {
 		fail("Not yet implemented");
 	}
 
-	/** Test method for {@link core.CheckersLogic#validateInput()}. */
 	@Test
 	void testValidateInput() {
 		fail("Not yet implemented");
 	}
 
-	/**
-	 * Test method for {@link core.CheckersLogic#validatePairs(java.lang.String[])}.
-	 */
 	@Test
 	void testValidatePairs() {
 		fail("Not yet implemented");
 	}
 
-	/** Test method for {@link core.CheckersLogic#isValidLetter(char)}. */
 	@Test
 	void testIsValidLetter() {
 		fail("Not yet implemented");
 	}
 
-	/** Test method for {@link core.CheckersLogic#isValidNumber(char)}. */
 	@Test
 	void testIsValidNumber() {
 		fail("Not yet implemented");
 	}
 
-	/** Test method for {@link core.CheckersLogic#getPlayer(char)}. */
-	@Test
-	void testGetPlayer() {
-		fail("Not yet implemented");
-	}
 }
